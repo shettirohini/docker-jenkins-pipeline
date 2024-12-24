23a23562151901f21baf3af7588db634b753cb6f
shettirohini <rohinipattanshetti02@gmail.com>
2024-12-24 22:01:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ def status():
     return {"status": "Application is running smoothly"}
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=6000)
+    app.run(host="0.0.0.0", port=5000)
 
